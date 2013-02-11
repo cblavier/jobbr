aa29fb24ef7292c23006f0d1f497aa647d797520
Christian Blavier <cblavier@gmail.com>
2013-02-11 16:07:51 +0100

    added Travis badge

--- spec/dummy/config/schedule.rb
+++ spec/dummy/config/schedule.rb
@@ -1,7 +1,5 @@
-# loading Rails environment
 ENV['RAILS_ENV'] = ARGV.first || ENV['RAILS_ENV'] || 'development'
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-
 require "jobbr/whenever"
 
 set :output, "#{path}/log/cron.log"
