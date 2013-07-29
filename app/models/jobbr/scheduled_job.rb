51dede034be7987390da41e3955c393684341bd3
Christian Blavier <cblavier@gmail.com>
2013-07-29 11:29:30 +0200

    trying to fix heroku issue

--- app/models/jobbr/scheduled_job.rb
+++ app/models/jobbr/scheduled_job.rb
@@ -1,43 +1,39 @@
-module Jobbr
+class Jobbr::ScheduledJob < Job
 
-  class ScheduledJob < Job
+  field :scheduled, type: Boolean, default: true
 
-    field :scheduled, type: Boolean, default: true
+  default_scope ->{ Job.where(scheduled: true, :_type.ne => nil) }
 
-    default_scope ->{ Job.where(scheduled: true, :_type.ne => nil) }
+  def perform
+    raise NotImplementedError.new :message => 'Must be implemented'
+  end
 
-    def perform
-      raise NotImplementedError.new :message => 'Must be implemented'
+  def self.every(every = nil, options = {})
+    @every = [every, options] if every
+    @every
+  end
+
+  # heroku frequency can be :minutely, :hourly or :daily
+  def self.heroku_run(frequency, options = {})
+    @heroku_frequency = frequency
+    @heroku_priority = options[:priority] || 0
+  end
+
+  def self.heroku_frequency
+    @heroku_frequency
+  end
+
+  def self.heroku_priority
+    @heroku_priority
+  end
+
+  def self.task_name(with_namespace = false)
+    task_name = name.demodulize.underscore
+    if with_namespace
+      "jobbr:#{task_name}"
+    else
+      task_name
     end
-
-    def self.every(every = nil, options = {})
-      @every = [every, options] if every
-      @every
-    end
-
-    # heroku frequency can be :minutely, :hourly or :daily
-    def self.heroku_run(frequency, options = {})
-      @heroku_frequency = frequency
-      @heroku_priority = options[:priority] || 0
-    end
-
-    def self.heroku_frequency
-      @heroku_frequency
-    end
-
-    def self.heroku_priority
-      @heroku_priority
-    end
-
-    def self.task_name(with_namespace = false)
-      task_name = name.demodulize.underscore
-      if with_namespace
-        "jobbr:#{task_name}"
-      else
-        task_name
-      end
-    end
-
   end
 
 end
