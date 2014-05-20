29e4f255617948d34da3379860376ecdc5dd08ba
Christian Blavier <cblavier@gmail.com>
2014-05-20 11:06:29 +0200

    specs for heroku rake tasks

---
+++ spec/tasks/jobbr_heroku_tasks_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'rake'
+
+describe 'jobbr_heroku_tasks' do
+
+  before do
+    Rake.application.rake_require 'tasks/jobbr_heroku_tasks'
+    Rake::Task.define_task(:environment)
+  end
+
+  it "run daily jobs" do
+    expect {
+      Rake.application.invoke_task 'jobbr:heroku:daily'
+    }.to change { Jobbr::Run.all.count }.from(0).to(1)
+  end
+
+end
