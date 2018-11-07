c2f397d375d74394de11b694bb5002771b81b9f8
Christian Blavier <cblavier@gmail.com>
2018-11-07 18:50:18 +0100

    disabled codeclimate for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
 
-if ENV['CI'] == 'true'
-  ENV['CODECLIMATE_REPO_TOKEN'] ||= 'edafaf863fa93aff340625ec4ac8d70244456849256d0b4b16383ca0a76a11ec'
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-end
+# if ENV['CI'] == 'true'
+#   ENV['CODECLIMATE_REPO_TOKEN'] ||= 'edafaf863fa93aff340625ec4ac8d70244456849256d0b4b16383ca0a76a11ec'
+#   require "codeclimate-test-reporter"
+#   CodeClimate::TestReporter.start
+# end
 
 require File.join(File.dirname(__FILE__), 'dummy', 'config', 'environment.rb')
 require 'require_all'
